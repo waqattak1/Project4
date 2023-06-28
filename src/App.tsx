45dbf0b0923d2b52f9d0107bb3a5a8ef00630e0d
waqattak1 <waqas.khattak2016@gmail.com>
2023-06-28 10:58:17 -0400

    Made some additional changes, still having issues rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,51 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import ProductList from "./components/ProductList/ProductList";
-import {Product} from "./components/Product/Product"; // Import the Product component
+import { Product } from "./components/Product/Product";
 import LoginPage from "./components/LoginPage/LoginPage";
 import NavBar from "./components/NavBar/NavBar";
-import { CartProvider } from "./contexts/CartProvider"; 
+import { CartProvider } from "./contexts/CartProvider";
 import { Cart } from './components/Cart/Cart';
+import { useState, useEffect } from 'react';
+import Checkout from "./components/Checkout/Checkout";
+
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsAuthenticated(!!token);
+  }, []);
+
+  const handleLogin = (token: string) => {
+    localStorage.setItem('token', token);
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsAuthenticated(false);
+  };
+
   return (
     <CartProvider>
       <Router>
-        <NavBar />
+        <NavBar handleLogout={handleLogout} isAuthenticated={isAuthenticated} />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/cart" element={<Cart />} />
+          {isAuthenticated && (
+            <>
+              <Route path="/products" element={<ProductList />} />
+              <Route path="/products/:id" element={<Product />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/checkout" element={<Checkout />} />
+
+            </>
+          )}
+          {!isAuthenticated && (
+            <Route path="/login" element={<LoginPage handleLogin={handleLogin} />} />
+          )}
         </Routes>
       </Router>
     </CartProvider>
@@ -24,4 +53,3 @@ function App() {
 }
 
 export default App;
-
